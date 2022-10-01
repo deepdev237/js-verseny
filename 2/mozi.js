a71588da95acbacd9946c9869ece2403787c74b5
deep237 <oppaagzt@gmail.com>
2022-10-02 01:52:26 +0200

    more movies and template

--- 2/mozi.js
+++ 2/mozi.js
@@ -1,5 +1,8 @@
 //filmek
 
+//template
+//movies.push({title: "", categories: ["", ""], relase_date: "", img: "", link: ""})
+
 var movies = []
 movies.push({title: "Mission: Impossible", categories: ["Akció", "Kaland", "Thriller"], relase_date: "1996. május 22.", img: "https://upload.wikimedia.org/wikipedia/en/e/e1/MissionImpossiblePoster.jpg", link: "https://www.imdb.com/title/tt0117060/"});
 movies.push({title: "Ready Player One", categories: ["Akció", "Sci-Fi"], relase_date: "2018. március 29.", img: "https://m.media-amazon.com/images/M/MV5BY2JiYTNmZTctYTQ1OC00YjU4LWEwMjYtZjkwY2Y5MDI0OTU3XkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_.jpg", link: "https://www.imdb.com/title/tt1677720/"});
@@ -7,6 +10,15 @@ movies.push({title: "The Suicide Squad", categories: ["Vígjáték", "Fantasy"],
 movies.push({title: "Don't Look Up", categories: ["Vígjáték", "Politikai szatíra"], relase_date: "2021. december 5.", img: "https://fr.web.img3.acsta.net/pictures/21/11/16/17/11/5656957.jpg", link: "https://www.imdb.com/title/tt11286314/"});
 movies.push({title: "Countdown", categories: ["Horror", "Thriller"], relase_date: "2019. október 25.", img: "https://m.media-amazon.com/images/M/MV5BODY3OGEyMTgtYTZjZi00Y2YzLWFjY2UtMjEwYWE1MjRkOTc4XkEyXkFqcGdeQXVyODQxMTI4MjM@._V1_.jpg", link: "https://www.imdb.com/title/tt10039344/"});
 movies.push({title: "No Time to Die", categories: ["Akció", "Thriller"], relase_date: "2021. szeptember 30.", img: "https://m.media-amazon.com/images/M/MV5BYWQ2NzQ1NjktMzNkNS00MGY1LTgwMmMtYTllYTI5YzNmMmE0XkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_.jpg", link: "https://www.imdb.com/title/tt2382320/"});
+movies.push({title: "Top Gun: Maverick", categories: ["Akció", "Dráma"], relase_date: "2022. május 26.", img: "https://m.media-amazon.com/images/M/MV5BZWYzOGEwNTgtNWU3NS00ZTQ0LWJkODUtMmVhMjIwMjA1ZmQwXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_FMjpg_UX1000_.jpg", link: "https://www.imdb.com/title/tt1745960/"})
+movies.push({title: "Buddy Games", categories: ["Vígjáték"], relase_date: "2019. február 10.", img: "https://m.media-amazon.com/images/M/MV5BNmE4ZTZmYjAtNGU2OS00NDFlLWI2OWYtMjYwZmVjOWJlOWE1XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg", link: "https://www.imdb.com/title/tt7070818/"})
+movies.push({title: "Love in the Villa", categories: ["Vígjáték", "Romantikus"], relase_date: "2022. szeptember 1.", img: "https://m.media-amazon.com/images/M/MV5BMjI5NmZmYTUtM2MzZS00ZmI0LTgyMzAtMTgwNWMyY2NjYjVkXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg", link: "https://www.imdb.com/title/tt15463032/"})
+movies.push({title: "Panama", categories: ["Akció", "Thriller"], relase_date: "2022.március 18.", img: "https://m.media-amazon.com/images/M/MV5BMWQ0Mjc5ZjAtZmRjNi00MGY4LWFlNDYtOGVlMjc3YjJiNTk4XkEyXkFqcGdeQXVyMTI3NDk1ODY0._V1_FMjpg_UX1000_.jpg", link: "https://www.imdb.com/title/tt4029412/"})
+movies.push({title: "Fall", categories: ["Thriller"], relase_date: "2022.augusztus 12.", img: "https://m.media-amazon.com/images/M/MV5BNGI3MWYwYjItNzZhYi00ZWIzLTkyMzYtN2JmNjg3ODg1NTg4XkEyXkFqcGdeQXVyMTMwMDA5ODU3._V1_FMjpg_UX1000_.jpg", link: "https://www.imdb.com/title/tt15325794/"})
+movies.push({title: "I Me Wed", categories: ["Vígjáték", "Romantikus"], relase_date: "2009. május 6.", img: "https://m.media-amazon.com/images/M/MV5BMTA5NTA5MTAyNjJeQTJeQWpwZ15BbWU3MDA2MTA1NTE@._V1_FMjpg_UX1000_.jpg", link: "https://www.imdb.com/title/tt0991164/"})
+movies.push({title: "Girl in Progress", categories: ["Vígjáték", "Dráma"], relase_date: "2012. május 11.", img: "https://m.media-amazon.com/images/M/MV5BMjEyMzk3MTgwNV5BMl5BanBnXkFtZTcwMTU3Njg2Nw@@._V1_FMjpg_UX1000_.jpg", link: "https://www.imdb.com/title/tt1817676/"})
+movies.push({title: "Demigod", categories: ["Horror"], relase_date: "2021. oktober 15.", img: "https://m.media-amazon.com/images/M/MV5BZDRhZWQzYTctNzQwNy00MjUzLWI1ZWUtNDdlNDQ5NWEzZDRlXkEyXkFqcGdeQXVyNDM1MDI0MjA@._V1_FMjpg_UX1000_.jpg", link: "https://www.imdb.com/title/tt12058582/"})
+movies.push({title: "The Dunes", categories: ["Thriller"], relase_date: "2021. október 21.", img: "https://m.media-amazon.com/images/M/MV5BNjE3OTJiNmQtZjBhOC00MzA0LWE2YzktNGQxOWI5ODA4OGZjXkEyXkFqcGdeQXVyMTI4NDM0MjE@._V1_FMjpg_UX1000_.jpg", link: "https://www.imdb.com/title/tt6910678/"})
 
 function GetLikedCategories(){
 	let x = $('input[name="like"]:checked').map(function(){return $(this).val();}).get();
