206e167cb711db78574d9a4dfecfbee82b146a3e
randomUSR56 <90837272+randomUSR56@users.noreply.github.com>
2022-10-01 21:36:59 +0200

    A képpörgetős részt rátok bízom..

---
+++ 1/script.js
@@ -0,0 +1,26 @@
+function tamasFunction() {
+    var text = document.getElementById("tamasText");
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+}
+
+function alexFunction() {
+    var text = document.getElementById("alexText");
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+}
+
+function daniFunction() {
+    var text = document.getElementById("daniText");
+    if (text.style.display === "block") {
+      text.style.display = "none";
+    } else {
+      text.style.display = "block";
+    }
+}
